201cf992092d96d847b6f04b82a3cf40ec42e73f
Kmmovies <sangu910836@gmail.com>
2026-02-20 10:57:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def start_handler(c, m):
     user_id = m.from_user.id
     if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
     lazydeveloper_btn = [[
-        InlineKeyboardButton('❤. Kannada Monsters .🍟', url='https://t.me/+i9BBTzFSEeAzZGI1')
+        InlineKeyboardButton('❤. Kannada Monsters .🍟', url='https://t.me/+sLmvrgSPppg3ODA1')
     ]]
     joinlink = f"https://t.me/+i9BBTzFSEeAzZGI1"
     return await m.reply_text(text=START_TEXT.format(m.from_user.mention, joinlink), disable_web_page_preview=True)
@@ -80,7 +80,7 @@ async def req_accept(c, m):
     await c.approve_chat_join_request(chat_id, user_id)
     try: 
         lazydeveloper_btn = [[
-        InlineKeyboardButton('❤. Kannada Monsters .🍟', url='https://t.me/+_WhibeG5KhI2NDJl')
+        InlineKeyboardButton('❤. Kannada Monsters .🍟', url='https://t.me/+sLmvrgSPppg3ODA1')
         ]] 
         joinlink = f"https://t.me/+_WhibeG5KhI2NDJl"
         await c.send_message(
@@ -99,5 +99,6 @@ Bot.run()
 
 
 #crafted by - the one and only LazyDeveloperr
+
 
 
